f047a50887e272a184bb38213c48b4e5aceba1b9
Sharon-B <sharonbagnall@gmail.com>
2020-12-04 18:53:50 +0000

    Fix so markers now display

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,9 @@
-
 function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 7,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 40.785091,
+            lng: -73.968285
         }
     });
 
@@ -23,10 +22,9 @@ function initMap() {
         });
     });
 
-  
-    import MarkerClusterer from '@google/markerclustererplus';
+    let markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
-    const markerCluster = new MarkerClusterer(map, markers);
 };
 
 /* var markerCluster = new MarkerClusterer(map, markers, {
