4f4860ab5f39dcc01ae5a5643ea2ff073f6b88dc
Sharon-B <sharonbagnall@gmail.com>
2020-12-06 17:37:55 +0000

    Fetch repo info for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -16,12 +16,39 @@ function userInformationHTML(user) {
         </div>`;
 }
 
+function repoInformationHTML(repos) { //As the object returned from our github api will be as an array so we can 
+    //use the standard .length array method to see if it's equal to zero. If it is our array is empty and there are no repos for that user.
+    if(repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+    // if there is data in the returned array we want to iterate through it, to do this we create a variable listItemsHTML 
+    //which will take the results of the map() method which will be run against our repos array. The map() behaves like a forEach but returns an array
+    // with the results of this function
+    let listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}   
+                </ul>
+            </div>`;
+
+            /*As map() returns an array we can use the join() method on that array and join everything with a new line "\n"
+            This stops us from having to iterate through the new array once again. */
+}
+
 function fetchGitHubInformation(event) {
 
     let username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
-        /*This h2 is didplayed if input field is empty ie not username*/
+        /*This h2 is displayed if input field is empty ie not username*/
         return; //We need to return out of this statement and not fetch data from github api if the input field is empty, so this returns us out of the function.
     }
   
@@ -41,11 +68,14 @@ $.when (
 */
 
 $.when(
-    $.getJSON(`https://api.github.com/users/${username}`) //.when takes a function as it's first argument, so we use the getJSON function to retrieve the data from the github api
-).then( // then we want to display it in our gh-user-data div:
-    function(response) { // so we use a function to pass in the respose and declare a userData variable to store it
-        let userData = response;
+    $.getJSON(`https://api.github.com/users/${username}`), //.when takes a function as it's first argument, so we use the getJSON function to retrieve the data from the github api
+    $.getJSON(`https://api.github.com/users/${username}/repos`) // add a second getJSON to retrieve the users repo data so it ca be displayed
+).then( // then we want to display it in our gh-user-data & gh-repo-data divs:
+    function(firstResponse, secondResponse) { // so we use a function to pass in the respose and declare a userData variable to store it
+        let userData = firstResponse[0]; //Now that we have 2 getJSON calls we will have  responses come back
+        let repoData = secondResponse[0]; // Now we will also have 2 variables one for each response. The when method will return these as arrays so we need to target the first item in each array using the index
         $("#gh-user-data").html(userInformationHTML(userData)); //set the html of the div to the results of the userInformationHTML function which is yet to be written and set the userData as the argument
+        $("#gh-repo-data").html(repoInformationHTML(repoData)); //set the html of the div to the results of the repoInformationHTML function  and set the repoData as the argument
     }, function(errorResponse) { //error function if it doesn't work
         if (errorResponse.status === 404) { // if not found
             $("#gh-user-data").html( // set html to error message
@@ -56,5 +86,4 @@ $.when(
                 `<h2.>Error: ${errorResponse.responseJSON.message}</h2.>`);
         }
     });
-
 }
